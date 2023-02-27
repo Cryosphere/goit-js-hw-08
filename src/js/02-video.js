1a17197a41bbae87bd9a99cac19d99f08ac43118
Sergey Shmatok <trycifer@gmail.com>
2023-02-28 00:17:19 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,15 +4,19 @@ import { throttle } from "lodash";
 const iframe= document.querySelector('iframe');
 const player = new Player(iframe);
 
-if (localStorage.getItem('videoplayer-current-time')===null){
-    localStorage.setItem('videoplayer-current-time', 0)
-};
+
 
 player.on('timeupdate', throttle(evt => {
     localStorage.setItem('videoplayer-current-time', evt.seconds);
 }, 1000)
 );
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(function(error) {
-    console.error(error);});
+if (localStorage.getItem('videoplayer-current-time')!==null){
+    player
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function (error) {
+    console.error(error)
+});
+};
+
 
