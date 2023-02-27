bde3060921959bd06a5db9dd4564f31ef3794dde
Sergey Shmatok <trycifer@gmail.com>
2023-02-27 19:25:57 +0200

    add list style

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,35 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach((item) => {
+  const galleryListItem = document.createElement('li');
+  galleryListItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = item.preview;
+  galleryImage.setAttribute('alt', item.description);
+  galleryImage.setAttribute('title', item.description);
+
+  galleryLink.appendChild(galleryImage);
+  galleryListItem.appendChild(galleryLink);
+  items.push(galleryListItem);
+});
+
+gallery.append(...items);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
